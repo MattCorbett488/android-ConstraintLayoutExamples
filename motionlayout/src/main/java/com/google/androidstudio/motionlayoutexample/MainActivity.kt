29011a1eef8445f7eab35353c8929d4996a00029
Matt Corbett <matthew.corbett@willowtreeapps.com>
2018-07-22 17:15:13 -0400

    Add OnClick example for MotionLayout

--- motionlayout/src/main/java/com/google/androidstudio/motionlayoutexample/MainActivity.kt
+++ motionlayout/src/main/java/com/google/androidstudio/motionlayoutexample/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener
     private val myDataset: Array<DemosAdapter.Demo> = arrayOf(
             DemosAdapter.Demo("Basic Example (1/2)", "Basic motion example using referenced ConstraintLayout files", R.layout.motion_01_basic),
             DemosAdapter.Demo("Basic Example (2/2)", "Basic motion example using ConstraintSets defined in the MotionScene file", R.layout.motion_02_basic),
+            DemosAdapter.Demo("OnClick Example", "Basic example showing the OnClick tag", R.layout.motion_onclick),
             DemosAdapter.Demo("Custom Attribute", "Show color interpolation (custom attribute)", R.layout.motion_03_custom_attribute),
             DemosAdapter.Demo("ImageFilterView (1/2)", "Show image cross-fade (using ML's ImageFilterView + custom attribute)", R.layout.motion_04_imagefilter),
             DemosAdapter.Demo("ImageFilterView (2/2)", "Show image saturation transition (using ML's ImageFilterView + custom attribute)", R.layout.motion_05_imagefilter),
